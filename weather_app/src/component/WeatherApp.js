8df528462072a171b64b98b2d0d181ae17a4ea28
Minxing Li <haydenlee6677@gmail.com>
2020-05-09 03:18:02 +1000

    5.8

--- weather_app/src/component/WeatherApp.js
+++ weather_app/src/component/WeatherApp.js
@@ -6,7 +6,7 @@ import WeatherAppJPX from './WeatherAppJPX.js';
 
 const API_key = '7abb2f96af69ff0a9164e92bc934932d';
 const city = 'Sydney';
-const apiF= `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}`;
+const apiF = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}`;
 const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`;
 
 class WeatherApp extends React.Component {
@@ -23,13 +23,18 @@ class WeatherApp extends React.Component {
             main: undefined,
             celsius: undefined,
             description: "",
-            error:false,
+            error: false,
+            
+            tempF: [],
+            iconF: [],
+            dateF: [],
+            descriptionF: [],
 
-            tempF:[],
-
+            background:{background:encodeURI('./background.jpg')},
 
         };
 
+        // store icons' ccs name
         this.weatherIcons = {
             Thunderstorm: "wi-thunderstorm",
             Drizzle: "wi-sleet",
@@ -41,57 +46,107 @@ class WeatherApp extends React.Component {
         };
     }
 
+    //change icon'id into icons' ccs name
     get_WeatherIcon(weatherIcons, rangeId) {
+        let weatherIcon;
         switch (true) {
             case rangeId >= 200 && rangeId < 232:
-                this.setState({ icon: weatherIcons.Thunderstorm });
+                weatherIcon = weatherIcons.Thunderstorm;
+
                 break;
             case rangeId >= 300 && rangeId <= 321:
-                this.setState({ icon: weatherIcons.Drizzle });
+                weatherIcon = weatherIcons.Drizzle;
+
                 break;
             case rangeId >= 500 && rangeId <= 521:
-                this.setState({ icon: weatherIcons.Rain });
+                weatherIcon = weatherIcons.Rain;
+
                 break;
             case rangeId >= 600 && rangeId <= 622:
-                this.setState({ icon: weatherIcons.Snow });
+                weatherIcon = weatherIcons.Snow;
+
                 break;
             case rangeId >= 701 && rangeId <= 781:
-                this.setState({ icon: weatherIcons.Atmosphere });
+                weatherIcon = weatherIcons.Atmosphere;
+
                 break;
             case rangeId === 800:
-                this.setState({ icon: weatherIcons.Clear });
+                weatherIcon = weatherIcons.Clear;
+
                 break;
             case rangeId >= 801 && rangeId <= 804:
-                this.setState({ icon: weatherIcons.Clouds });
+                weatherIcon = weatherIcons.Clouds;
                 break;
             default:
-                this.setState({ icon: weatherIcons.Clear });
+
         }
+        return weatherIcon;
+    }
+
+    //change original data into date
+    get_Date(str) {
+        let dateStr, monthStr, dayStr;
+
+
+
+        switch (str.substr(0, 2)) {
+            case '01':
+                monthStr = 'Jan';
+                break;
+            case '02':
+                monthStr = 'Feb';
+                break;
+            case '03':
+                monthStr = 'Mar';
+                break;
+            case '04':
+                monthStr = 'Apr';
+                break;
+            case '05':
+                monthStr = 'May';
+                break;
+            case '06':
+                monthStr = 'Jun';
+                break;
+            case '07':
+                monthStr = 'Jul';
+                break;
+            case '08':
+                monthStr = 'Aug';
+                break;
+            case '09':
+                monthStr = 'Sep';
+                break;
+            case '10':
+                monthStr = 'Oct';
+                break;
+            case '11':
+                monthStr = 'Nov';
+                break;
+            case '12':
+                monthStr = 'Dec';
+                break;
+        }
+
+        if (str[3] == '1') {
+            dayStr = str.substr(3);
+        } else {
+            dayStr = str.substr(4);
+        }
+
+        dateStr = `${monthStr}  ${dayStr}`;
+
+        return dateStr;
     }
 
     async componentDidMount() {
+
+        // function: get today weather
         const response = await (await fetch(api)).json();
         // const response=await fetch(api);
-        const responseF = await (await fetch(apiF)).json();
 
+        console.log("Today:");
         console.log(response);
-        console.log(responseF);
-        
-        const obj={};
-        
-        obj[0]=responseF.list[7];
-        obj[1]=responseF.list[15];
-        obj[2]=responseF.list[23];
-        obj[3]=responseF.list[31];
-        obj[4]=responseF.list[39];
-        console.log(obj);
-
-        // for(let i=0;i<=4;i++){
-        //     tempF.push()
-        // }
-
-        
-
 
         // const {city:{name},city:{sunrise}}=response;
         // //const sunrise=response.city.sunrise;
@@ -105,45 +160,109 @@ class WeatherApp extends React.Component {
             temp_max: this.calculateCelsius(response.main.temp_max),
             temp_min: this.calculateCelsius(response.main.temp_min),
             description: response.weather[0].description,
+            icon: this.get_WeatherIcon(this.weatherIcons, response.weather[0].id),
         });
 
-        this.get_WeatherIcon(this.weatherIcons, response.weather[0].id);
 
 
-        // for(let i=7,j=0;i<=40;i=i+8){
-        //     this.setState({
-        //         tempF[j]:responseF.list[0].main.temp,           
-        //      });
+        //function: forecast coming five days
+        const responseF = await (await fetch(apiF)).json();
+        console.log('Forecast:');
+        console.log(responseF);
+        const obj = {};
+        const tempFArray = [], iconFArray = [], dateFArray = [], descriptionFArray = [];
+        obj[0] = responseF.list[7];
+        obj[1] = responseF.list[15];
+        obj[2] = responseF.list[23];
+        obj[3] = responseF.list[31];
+        obj[4] = responseF.list[39];
+        console.log('Filled forecast:');
+        console.log(obj);
 
-        // }
+        for (let i = 0; i <= 4; i++) {
+            tempFArray.push(this.calculateCelsius(obj[i].main.temp));
+            descriptionFArray.push(obj[i].weather[0].description);
+            iconFArray.push(this.get_WeatherIcon(this.weatherIcons, obj[i].weather[0].id));
+            dateFArray.push(this.get_Date(obj[i].dt_txt.slice(5, 10)));
+        }
+        this.setState({
+            tempF: tempFArray,
+            descriptionF: descriptionFArray,
+            iconF: iconFArray,
+            dateF: dateFArray,
+        });
+
+
+
+
+
 
     }
 
     loadWeather = async e => {
         e.preventDefault();
-        
-        const city = e.target.elements.city.value;
-        const country = e.target.elements.country.value;
-        const newapi = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`;
 
-        const response = await (await fetch(newapi)).json();
-        console.log(response);
+        const newcity = e.target.elements.city.value;
+        const newapi = `http://api.openweathermap.org/data/2.5/weather?q=${newcity}&appid=${API_key}`;
+        const newapiF = `http://api.openweathermap.org/data/2.5/forecast?q=${newcity}&appid=${API_key}`;
 
-        if(city&&country){
-        this.setState({
-            isLoading: false,
-            city: response.name,
-            country: response.sys.country,
-            temp: this.calculateCelsius(response.main.temp),
-            temp_max: this.calculateCelsius(response.main.temp_max),
-            temp_min: this.calculateCelsius(response.main.temp_min),
-            description: response.weather[0].description,
-        });
-        this.get_WeatherIcon(this.weatherIcons, response.weather[0].id);
-    }else{
-        this.setState({error:true});
-        alert("Hello! I am an alert box!!");
-    }
+
+
+
+        if (newcity) {
+
+
+            const response = await (await fetch(newapi)).json();
+            console.log("Today:");
+            console.log(response);
+
+            if (response.cod == 404) {
+                alert("Please input correct city name!");
+            } else {
+                this.setState({
+                    isLoading: false,
+                    city: response.name,
+                    country: response.sys.country,
+                    temp: this.calculateCelsius(response.main.temp),
+                    temp_max: this.calculateCelsius(response.main.temp_max),
+                    temp_min: this.calculateCelsius(response.main.temp_min),
+                    description: response.weather[0].description,
+                    icon: this.get_WeatherIcon(this.weatherIcons, response.weather[0].id),
+                });
+
+
+                //function: forecast coming five days
+                const responseF = await (await fetch(newapiF)).json();
+                console.log('Forecast:');
+                console.log(responseF);
+                const obj = {};
+                const tempFArray = [], iconFArray = [], dateFArray = [], descriptionFArray = [];
+                obj[0] = responseF.list[7];
+                obj[1] = responseF.list[15];
+                obj[2] = responseF.list[23];
+                obj[3] = responseF.list[31];
+                obj[4] = responseF.list[39];
+                console.log('Filled forecast:');
+                console.log(obj);
+
+                for (let i = 0; i <= 4; i++) {
+                    tempFArray.push(this.calculateCelsius(obj[i].main.temp));
+                    descriptionFArray.push(obj[i].weather[0].description);
+                    iconFArray.push(this.get_WeatherIcon(this.weatherIcons, obj[i].weather[0].id));
+                    dateFArray.push(this.get_Date(obj[i].dt_txt.slice(5, 10)));
+                }
+                this.setState({
+                    tempF: tempFArray,
+                    descriptionF: descriptionFArray,
+                    iconF: iconFArray,
+                    dateF: dateFArray,
+                });
+            }
+
+        } else {
+            this.setState({ error: true });
+            alert("Please input city name!");
+        }
 
     }
 
@@ -168,6 +287,11 @@ class WeatherApp extends React.Component {
                 temp_max={this.state.temp_max}
                 temp_min={this.state.temp_min}
                 description={this.state.description}
+                tempF={this.state.tempF}
+                descriptionF={this.state.descriptionF}
+                iconF={this.state.iconF}
+                dateF={this.state.dateF}
+                background={this.state.background}
             />
 
 
--- weather_app/src/component/WeatherAppJPX.js
+++ weather_app/src/component/WeatherAppJPX.js
@@ -1,17 +1,33 @@
 import React from "react";
 import './weather.css';
+//import imgurl from './background.jpg';
+import clearImgUrl from './clear.jpg';
+import cloudsImgUrl from './clouds.jpeg';
+import drizzleImgUrl from './drizzle.jpeg';
+import rainImgUrl from './rain.jpeg';
+import snowImgUrl from './snow.jpeg';
+import thunderstormImgUrl from './thunderstorm.jpeg';
+import atmosphereImgUrl from './atmosphere.jpeg';
+
+
 
 const WeatherAppJPX = (props) => {
-
+        
+    const url='url(' +clearImgUrl+ ')';
+    const a='background-size';
+    const c={
+        background:url,
+        a: 'contain',
+        };
     return (
 
-        <div className='main'>
-            <div className='card'>
-                <div className='card_top'>
+        <div className='main' >
+            <div className='card'  >
+                <div className='card_top' >
                     <div className='card_top_item'>
                         <i className={`wi ${props.icon} icon`}></i>
                         <h3>{props.description}</h3>
-                        {props.city},{props.country}
+                        {props.city}{props.country}
 
                     </div>
                     <div className='card_top_item'>
@@ -23,32 +39,70 @@ const WeatherAppJPX = (props) => {
                         <span> {props.temp_min}&deg;</span>
                     </div>
                     <div className='card_top_item'>
-                    <form onSubmit={props.loadWeather}>
-                        <input type='text' className='input' name='city' autoComplete='off' placeholder='city'/>
-                        <br />
-                        <input type='text' className='input' name='country' autoComplete='off' placeholder='country'/>
-                        <br />
-                        <button className='button'> Check Weather</button>
+                        <form onSubmit={props.loadWeather}>
+                            <input type='text' className='input' name='city' autoComplete='off' placeholder='city' />
+                            <br />
+
+                            <br />
+                            <button className='button'> Check Weather</button>
                         </form>
                     </div>
                 </div>
                 <div className='card_bottom'>
                     <div className='card_bottom_item'>
+                        <br />
+                        <span> {props.dateF[0]}</span>
+                        <br />
+                        <i className={`wi ${props.iconF[0]} `}></i>
+                        <br />
+                        <span> {props.tempF[0]}&deg;</span>
+                        <br />
+                        <span> {props.descriptionF[0]}</span>
 
                     </div>
                     <div className='card_bottom_item'>
+                        <br />
+                        <span> {props.dateF[1]}</span>
+                        <br />
+                        <i className={`wi ${props.iconF[1]} `}></i>
+                        <br />
+                        <span> {props.tempF[1]}&deg;</span>
+                        <br />
+                        <span> {props.descriptionF[1]}</span>
 
                     </div>
                     <div className='card_bottom_item'>
+                        <br />
+                        <span> {props.dateF[2]}</span>
+                        <br />
+                        <i className={`wi ${props.iconF[2]} `}></i>
+                        <br />
+                        <span> {props.tempF[2]}&deg;</span>
+                        <br />
+                        <span> {props.descriptionF[2]}</span>
 
                     </div>
                     <div className='card_bottom_item'>
-
+                        <br />
+                        <span> {props.dateF[3]}</span>
+                        <br />
+                        <i className={`wi ${props.iconF[3]} `}></i>
+                        <br />
+                        <span> {props.tempF[3]}&deg;</span>
+                        <br />
+                        <span> {props.descriptionF[3]}</span>
                     </div>
                     <div className='card_bottom_item'>
-
+                        <br />
+                        <span> {props.dateF[4]}</span>
+                        <br />
+                        <i className={`wi ${props.iconF[4]} `}></i>
+                        <br />
+                        <span> {props.tempF[4]}&deg;</span>
+                        <br />
+                        <span> {props.descriptionF[4]}</span>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
