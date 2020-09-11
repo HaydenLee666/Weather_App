eaca24b2dddb70f35bcd2d8c4cad37a3d28ceb49
Minxing Li <haydenlee6677@gmail.com>
2020-09-11 22:19:57 +1000

    hide api key

--- src/component/WeatherApp.js
+++ src/component/WeatherApp.js
@@ -3,7 +3,7 @@ import Weather from './Weather.js';
 import './WeatherApp.scss';
 
 
-const API_key = '7abb2f96af69ff0a9164e92bc934932d';
+const API_key = process.env.REACT_APP_WEATHER_API_KEY;
 
 class WeatherApp extends React.Component {
     constructor() {
